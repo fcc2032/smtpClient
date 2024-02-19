a180651e1541b60cac94c6f1f1f5daeaab8e2c82
fcc2032 <fcc2032@nyu.edu>
2024-02-19 16:59:00 -0500

    sdadfsdfsdf

--- smtpClient.py
+++ smtpClient.py
@@ -29,7 +29,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    mailCommand = 'MAIL FROM:<local>\r\n'
+    mailCommand = 'MAIL FROM:<local@local>\r\n'
     clientSocket.send(mailCommand.encode())
     
     #recv2 = clientSocket.recv(2048)
