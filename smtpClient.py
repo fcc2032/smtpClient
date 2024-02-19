6e45c203e694b32e500510d2e564d7b19c5a168b
fcc2032 <fcc2032@nyu.edu>
2024-02-19 17:14:24 -0500

    ok forgot to receive teh message so theTCP handshake didn't finish.

--- smtpClient.py
+++ smtpClient.py
@@ -14,7 +14,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.connect((mailserver, port))
     # Fill in end
 
-    recv = clientSocket.recv(1024).decode()
+    #recv = clientSocket.recv(1024).decode()
     #print(recv) #You can use these print statement to validate return codes from the server.
     #if recv[:3] != '220':
     #    print('220 reply not received from server.')
@@ -22,26 +22,28 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    #recv1 = clientSocket.recv(1024).decode()
+    clientSocket.recv(1024).decode()
     #print(recv1) 
     #if recv1[:3] != '250':
     #    print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
-    mailCommand = 'MAIL FROM:fcc2032@nyu.edu\r\n'
+    mailCommand = 'MAIL FROM:<fcc2032@nyu.edu>\r\n'
     clientSocket.send(mailCommand.encode())
+    clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
-    rcptCommand = 'RCPT TO:me@localhost\r\n'
+    rcptCommand = 'RCPT TO:<Postmaster>\r\n'
     clientSocket.send(rcptCommand.encode())
+    clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
-    dataCommand = 'DATA\r\n'
+    dataCommand = 'DATA \r\n'
     clientSocket.send(dataCommand.encode())
     #recv2 = clientSocket.recv(1024).decode()
     #print(recv2)
@@ -64,6 +66,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
+    clientSocket.recv(1024).decode()
     #recv3 = clientSocket.recv(1024).decode()
     #print(recv3)
     
