861d4eea7620673314497489c5f7e1f5b963fb78
fcc2032 <fcc2032@nyu.edu>
2024-02-19 16:57:46 -0500

    asdfa

--- smtpClient.py
+++ smtpClient.py
@@ -78,8 +78,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quitCommand = 'QUIT \r\n'
     clientSocket.send(quitCommand.encode())
-    recv3 = clientSocket.recv(1024).decode()
-    print(recv3)
+    #recv3 = clientSocket.recv(1024).decode()
+    #print(recv3)
     #for e in recv3:
     #    print(e)
     # Fill in end
